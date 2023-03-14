f8f486893de9362222041a94b9f39f1b707e51f1
EffenrilAgung <effenrilagung300@gmail.com>
2023-03-15 04:10:26 +0700

    complete

--- src/handler.js
+++ src/handler.js
@@ -76,21 +76,6 @@ const addBookHandler = (request, h) => {
   return response;
 };
 
-// const getAllBookHandler = (request, h) => {
-//   const response = h.response({
-//     status: 'success',
-//     data: {
-//       books: books.map((book) => ({
-//         id: book.id,
-//         name: book.name,
-//         publisher: book.publisher,
-//       })),
-//     },
-//   });
-
-//   response.code(200);
-//   return response;
-// };
 const getAllBookHandler = (request, h) => {
   const { name, reading, finished } = request.query;
   let findBook = books;
